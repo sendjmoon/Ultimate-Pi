c0743ae14a01df5e47fc5fde96c69dd746643b8f
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-09-12 21:16:49 -0700

    added dummy array for data to render on remote.html

--- app/controllers/remote-controller.js
+++ app/controllers/remote-controller.js
@@ -3,5 +3,25 @@
 module.exports = function(app) {
   app.controller('RemoteController', ['$http', function($http) {
     console.log('remote controller');
+    this.buttons = [
+      {
+        remoteId: 1,
+        remoteName: 'VIZIO',
+        name: 'KEY_POWER',
+        command: 'SEND_ONCE'
+      },
+      {
+        remoteId: 1,
+        remoteName: 'VIZIO',
+        name: 'VOL_DOWN',
+        command: 'SEND_ONCE'
+      },
+      {
+        remoteId: 1,
+        remoteName: 'VIZIO',
+        name: 'NUM_1',
+        command: 'SEND_ONCE'
+      },
+    ];
   }]);
 };
