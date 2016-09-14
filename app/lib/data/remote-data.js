77e57b310e04c9ab6533d8aa2c38ce532c9d0f73
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-09-14 16:26:14 -0700

    added styling for buttons on remote page. also modified layout on scss to prevent conflict when new pages render

--- app/lib/data/remote-data.js
+++ app/lib/data/remote-data.js
@@ -2,198 +2,203 @@
 
 let buttons = [
   {
-    'button': 'POWER',
-    'displayName': 'PWR'
+    button: 'POWER',
+    displayName: 'ON',
+    class: 'fa fa-power-off'
+  },
+  {
+    button: 'INFO',
+    displayName: 'INFO',
+    class: 'fa fa-info-circle'
+  },
+  {
+    button: 'VOLUMEDOWN',
+    displayName: 'VOL',
+    class: 'fa fa-chevron-circle-down'
+  },
+  {
+    button: 'VOLUMEUP',
+    displayName: 'VOL',
+    class: 'fa fa-chevron-circle-up'
+  },
+  {
+    button: 'MUTE',
+    displayName: 'MUTE',
+    class: 'fa fa-hard-of-hearing'
+  },
+  {
+    button: 'CHANNELUP',
+    displayName: 'CH',
+    class: 'fa fa-caret-square-o-up'
+  },
+  {
+    button: 'CHANNELDOWN',
+    displayName: 'CH',
+    class: 'fa fa-caret-square-o-down'
+  },
+  {
+    button: 'MODE',
+    displayName: 'MODE',
+    class: 'fa fa-circle-o'
   },
 
   {
-    'button': 'INFO',
-    'displayName': 'PWR'
-  },
-
-
-  {
-    'button': 'VOLUMEDOWN',
-    'displayName': 'VOL-'
-  },
-
-
-  {
-    'button': 'VOLUMEUP',
-    'displayName': 'VOL+'
-  },
-
-
-  {
-    'button': 'MUTE',
-    'displayName': '?'
-  },
-
-  {
-    'button': 'CHANNELUP',
-    'displayName': '/\\'
-  },
-
-  {
-    'button': 'MODE',
-    'displayName': '?'
-  },
-
-  {
-    'button': 'MENU',
-    'displayName': '?'
+    button: 'MENU',
+    displayName: 'MENU',
+    class: 'fa fa-bars'
   },
   {
-    'button': 'ENTER',
-    'displayName': '?'
+    button: 'ENTER',
+    displayName: 'ENTER',
+    class: 'fa fa-sign-in'
   },
   {
-    'button': 'OKAY',
-    'displayName': '?'
+    button: 'OKAY',
+    displayName: 'OKAY',
+    class: 'fa fa-check-square'
   },
   {
-    'button': 'BACK',
-    'displayName': '?'
+    button: 'BACK',
+    displayName: 'BACK',
+    class: 'fa fa-undo'
   },
   {
-    'button': 'EXIT',
-    'displayName': '?'
+    button: 'EXIT',
+    displayName: 'EXIT',
+    class: 'fa fa-sign-out'
   },
   {
-    'button': 'LAST',
-    'displayName': '?'
+    button: 'LAST',
+    displayName: 'LAST',
+    class: 'fa fa-hand-o-left'
   },
   {
-    'button': 'UP',
-    'displayName': '?'
+    button: 'UP',
+    displayName: 'UP',
+    class: 'fa fa-angle-double-up'
   },
   {
-    'button': 'DOWN',
-    'displayName': '?'
+    button: 'DOWN',
+    displayName: 'DOWN',
+    class: 'fa fa-angle-double-down'
   },
   {
-    'button': 'LEFT',
-    'displayName': '?'
+    button: 'LEFT',
+    displayName: 'LEFT',
+    class: 'fa fa-angle-double-left'
   },
   {
-    'button': 'RIGHT',
-    'displayName': '?'
+    button: 'RIGHT',
+    displayName: 'RIGHT',
+    class: 'fa fa-angle-double-right'
   },
   {
-    'button': 'SPACE',
-    'displayName': '?'
+    button: 'SPACE',
+    displayName: 'SPACE',
+    class: 'fa fa-space-shuttle'
   },
   {
-    'button': 'NUMERIC_1',
-    'displayName': '?'
+    button: 'NUMERIC_0',
+    displayName: '0'
   },
   {
-    'button': 'MODE',
-    'displayName': '?'
-  },
-
-  {
-    'button': 'NUMERIC_1',
-    'displayName': '1'
+    button: 'NUMERIC_1',
+    displayName: '1'
   },
   {
-    'button': 'NUMERIC_2',
-    'displayName': '2'
+    button: 'NUMERIC_2',
+    displayName: '2'
   },
-
-
   {
-    'button': 'NUMERIC_3',
-    'displayName': '3'
+    button: 'NUMERIC_3',
+    displayName: '3'
   },
-
   {
-    'button': 'NUMERIC_4',
-    'displayName': '4'
+    button: 'NUMERIC_4',
+    displayName: '4'
+  },
+  {
+    button: 'NUMERIC_5',
+    displayName: '5'
+  },
+  {
+    button: 'NUMERIC_6',
+    displayName: '6'
+  },
+  {
+    button: 'NUMERIC_7',
+    displayName: '7'
+  },
+  {
+    button: 'NUMERIC_8',
+    displayName: '8'
+  },
+  {
+    button: 'NUMERIC_9',
+    displayName: '9'
+  },
+  {
+    button: 'N',
+    displayName: 'NETFLIX',
+    class: 'fa fa-film'
   },
 
   {
-    'button': 'NUMERIC_5',
-    'displayName': '5'
+    button: 'A',
+    displayName: 'AMAZON',
+    class: 'fa fa-amazon'
   },
 
   {
-    'button': 'NUMERIC_6',
-    'displayName': '6'
-  },
-
-  {
-    'button': 'NUMERIC_7',
-    'displayName': '7'
-  },
-
-  {
-    'button': 'NUMERIC_8',
-    'displayName': '8'
-  },
-
-  {
-    'button': 'NUMERIC_9',
-    'displayName': '9'
-  },
-
-  {
-    'button': 'N',
-    'displayName': 'Netflix'
-  },
-
-  {
-    'button': 'A',
-    'displayName': 'Amazon'
-  },
-
-  {
-    'button': 'PLAY',
-    'displayName': 'TRIANGLE'
-  },
-
-
-  {
-    'button': 'PAUSE',
-    'displayName': 'II'
-  },
-
-  {
-    'button': 'STOP',
-    'displayName': 'RECTANGLE'
+    button: 'PLAY',
+    displayName: 'PLAY',
+    class: 'fa fa-play-circle'
   },
   {
-    'button': 'RECORD',
-    'displayName': 'RED SQUARE'
+    button: 'PAUSE',
+    displayName: 'PAUSE',
+    class: 'fa fa-pause-circle'
   },
   {
-    'button': 'PROGRAM',
-    'displayName': '?'
+    button: 'STOP',
+    displayName: 'STOP',
+    class: 'fa fa-stop-circle'
   },
   {
-    'button': 'FORWARD',
-    'displayName': '>>'
+    button: 'RECORD',
+    displayName: 'REC',
+    class: 'fa fa-video-camera'
   },
   {
-    'button': 'REWIND',
-    'displayName': '<<'
+    button: 'PROGRAM',
+    displayName: 'PRGM',
+    class: 'fa fa-product-hunt'
   },
-
-
   {
-    'button': 'SWITCHVIDEOMODE',
-    'displayName': '?'
+    button: 'FORWARD',
+    displayName: 'FWD',
+    class: 'fa fa-forward'
   },
-
   {
-    'button': 'SCREEN',
-    'displayName': '?'
+    button: 'REWIND',
+    displayName: 'RWD',
+    class: 'fa fa-backward'
   },
-
   {
-    'button': 'RADIO',
-    'displayName': '?'
+    button: 'SWITCHVIDEOMODE',
+    displayName: 'INPUT',
+    class: 'fa fa-clone'
+  },
+  {
+    button: 'SCREEN',
+    displayName: 'SCRN',
+    class: 'fa fa-desktop'
+  },
+  {
+    button: 'RADIO',
+    displayName: 'RADIO',
+    class: 'fa fa-headphones'
   }
 ];
 
-module.exports = buttons; 
+module.exports = buttons;
